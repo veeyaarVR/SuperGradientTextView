0e13cc015e8c241450cff9096dae6930e017993b
VeeyaaR <vigneshmarimuthu2302@gmail.com>
2019-09-18 08:04:21 +0530

    added start color end color option

--- supergradienttextview/src/main/java/com/veeyaar/supergradienttextview/GradientTextView.java
+++ supergradienttextview/src/main/java/com/veeyaar/supergradienttextview/GradientTextView.java
@@ -2,6 +2,7 @@ package com.veeyaar.supergradienttextview;
 
 import android.annotation.SuppressLint;
 import android.content.Context;
+import android.content.res.TypedArray;
 import android.graphics.Color;
 import android.graphics.LinearGradient;
 import android.graphics.Paint;
@@ -14,16 +15,35 @@ import androidx.annotation.Nullable;
 @SuppressLint("AppCompatCustomView")
 public class GradientTextView extends TextView {
 
+    private boolean isVertical;
+    private int startColor = Color.BLUE;
+    private int endColor = Color.GREEN;
+
     public GradientTextView(Context context) {
         super(context);
+        init(context, null);
+
     }
 
     public GradientTextView(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
+        init(context, attrs);
     }
 
     public GradientTextView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+        init(context, attrs);
+
+    }
+
+    private void init(Context context, AttributeSet attrs) {
+        if (attrs != null) {
+            final TypedArray attributes = context.obtainStyledAttributes(attrs, R.styleable.GradientTextView);
+            isVertical = attributes.getBoolean(R.styleable.GradientTextView_isVertical, false);
+            startColor = attributes.getColor(R.styleable.GradientTextView_endColor, startColor);
+            endColor = attributes.getColor(R.styleable.GradientTextView_startColor, endColor);
+            attributes.recycle();
+        }
     }
 
     @SuppressLint("DrawAllocation")
@@ -34,10 +54,17 @@ public class GradientTextView extends TextView {
             Paint paint = getPaint();
             float width = paint.measureText(getText().toString());
 
-            getPaint().setShader(new LinearGradient(0, 0, width, getLineHeight(),
-                    Color.BLUE,
-                    Color.YELLOW,
-                    Shader.TileMode.CLAMP));
+            if (isVertical) {
+                getPaint().setShader(new LinearGradient(0, 0, width, getLineHeight(),
+                        endColor,
+                        startColor,
+                        Shader.TileMode.CLAMP));
+            } else {
+                getPaint().setShader(new LinearGradient(0, 0, 0, getLineHeight(),
+                        endColor,
+                        startColor,
+                        Shader.TileMode.CLAMP));
+            }
         }
     }
 }
